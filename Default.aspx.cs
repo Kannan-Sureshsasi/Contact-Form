88c0976c0070927c5f3169b8d9bee00c97f136ad
Kannan S <138990724+Kannan-Sureshsasi@users.noreply.github.com>
2023-09-06 00:34:11 +0530

    Update Default.aspx.cs

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
-using System.Configuration;
-using System.Drawing;
 using System.Net;
 using System.Net.Mail;
-using System.Text;
 
 namespace Contact_Form
 {
